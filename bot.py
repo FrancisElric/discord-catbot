7d8f4e400c09a188d4090dec2f26b891d9f3680b
Francis Elric <mieszko.sadek@gmail.com>
2025-11-20 15:46:18 +0100

    added count of bites that will be stored in csv file, also I've updated gitignore file

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import datetime
+import csv
 
 import discord
 from discord.ext import commands
@@ -14,6 +15,13 @@ logging.basicConfig(filename='logs',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.DEBUG)
 
+# Load bites.csv to memory
+bites = dict()
+with open('bites.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        bites.update({row['user']: row['count_of_bites']})
+    print(bites)
 
 @bot.event
 async def on_ready():
@@ -25,19 +33,25 @@ async def on_ready():
 
 @bot.command()
 async def ugryź(ctx, user_id = None):
-    channel = ctx.channel
+    # channel = ctx.channel
     flavour_text = ''
     emoji = '😼'
     if random.randint(1,10) < 3:
         user_id = ctx.author
         flavour_text = 'Próbował_ś kogoś ugryźć, ale się nie udało! Wirus ugryzł Cb!\n'
     elif user_id is None:
-        user_id = random.choice(channel.guild.members)
+        user_id = random.choice(ctx.guild.members)
         print(user_id, bot.user)
         if user_id == bot.user:
             flavour_text = 'Coś poszło nie tak, ten gamoń ugryzł sam sb! \n'
             emoji = '🙀'
-    await channel.send(f'{flavour_text}ugryzłem {user_id.mention} {emoji}')
+    if user_id in bites.keys():
+        bites[user_id] += 1
+    else:
+        bites.update({user_id : 1})
+    # I tutaj powinno zapisać do pliku
+    await ctx.send(f'{flavour_text}ugryzłem {user_id.mention} {emoji}')
+    await ctx.send(f'total bites {bites[user_id]}')
 
 if __name__ == "__main__":
     bot.run(credentials.BOT_TOKEN)
